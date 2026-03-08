52387e2c6717f278f6178449537908300e1f2a7e
Daniel Murphy <rdanielmurphy@gmail.com>
2026-03-07 20:00:43 -0600

    cleanup 2

--- app/my-app/constants/config.js
+++
@@ -1,4 +0,0 @@
-// Change this to your server's IP/URL
-// For local dev with Expo Go on a physical device, use your computer's local IP
-export const API_URL = 'http://10.0.2.2:3000';
-export const SOCKET_URL = 'http://10.0.2.2:3000';
